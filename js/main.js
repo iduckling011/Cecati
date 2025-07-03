09d155c93e7781af5d765de798f55b0765d3291d
Ian Rodriguez <rodriguez.ian.6120@gmial.com>
2025-07-03 14:17:45 -0600

    second commit

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     try {
-      const res = await fetch('https://TU-BACKEND.onrender.com/api/contacto', {
+      const res = await fetch('https://inscripciones-cecati.onrender.com/api/contacto', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
